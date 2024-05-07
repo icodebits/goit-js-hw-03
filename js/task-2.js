2b5ffe3f5bd5e22c4a2fe5b19b8172812d9780c4
Serghei Filimonov <filimonov.sergey@gmail.com>
2024-05-07 15:43:45 +0300

    Improve task 2.

--- js/task-2.js
+++ js/task-2.js
@@ -1,7 +1,5 @@
 function makeArray(firstArray, secondArray, maxLength) {
-    let newArray = firstArray.slice(0);
-    
-    newArray.push(...secondArray);
+    let newArray = firstArray.concat(secondArray);
     
     if (newArray.length > maxLength) {
         return newArray.slice(0, maxLength);
@@ -18,4 +16,3 @@ console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); //
 console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
 console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
 console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
-
